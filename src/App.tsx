d3a192201ba10744a82dffeddc249eca9484580f
Yogesh Datir <yogdat@gmail.com>
2022-07-11 13:28:20 +0530

    added font

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { ChakraProvider, Box, extendTheme } from "@chakra-ui/react";
 import SignUp from "./pages/SignUp";
+import "@fontsource/poppins";
 
 const variantOutlined = () => ({
   field: {
@@ -11,6 +12,10 @@ const variantOutlined = () => ({
 });
 
 const theme = extendTheme({
+  fonts: {
+    heading: `'Poppins', sans-serif`,
+    body: `'Poppins', sans-serif`,
+  },
   components: {
     Input: {
       variants: {
